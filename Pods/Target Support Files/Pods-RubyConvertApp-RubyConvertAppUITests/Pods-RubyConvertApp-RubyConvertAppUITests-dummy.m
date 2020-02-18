a802f7133a0b46e9c923a951cad507468742ff30
muta <muta@m.cs.osakafu-u.ac.jp>
2020-02-19 06:41:02 +0900

    feat: add Podfile

---
+++ Pods/Target Support Files/Pods-RubyConvertApp-RubyConvertAppUITests/Pods-RubyConvertApp-RubyConvertAppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RubyConvertApp_RubyConvertAppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_RubyConvertApp_RubyConvertAppUITests
+@end
