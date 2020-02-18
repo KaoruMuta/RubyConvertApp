795a362bc41bca546690256d93eccbdc0a01c1f8
muta <muta@m.cs.osakafu-u.ac.jp>
2020-02-19 05:28:04 +0900

    first commit

---
+++ RubyConvertApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RubyConvertApp
+//
+//  Created by k_muta on 2020/02/19.
+//  Copyright © 2020 muta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
