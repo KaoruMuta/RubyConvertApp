5e0e75dee7f58a63354d52cf4721d4c63c328ff5
muta <muta@m.cs.osakafu-u.ac.jp>
2020-02-23 15:31:36 +0900

    feat: create service to get the response from GooAPI

--- RubyConvertApp/Models/API/GooAPI.swift
+++ RubyConvertApp/Models/API/GooAPI.swift
@@ -7,3 +7,40 @@
 //
 
 import Foundation
+import Moya
+
+enum GooAPI {
+    case search(request: Dictionary<String, Any>)
+}
+
+extension GooAPI: TargetType {
+    var baseURL: URL {
+        return URL(string: Parameters.baseURL)!
+    }
+    
+    var path: String {
+        switch self {
+        case .search:
+            return Parameters.requestPath
+        }
+    }
+    
+    var method: Moya.Method {
+        return .post
+    }
+    
+    var sampleData: Data {
+        return Data()
+    }
+    
+    var task: Task {
+        switch self {
+        case .search(let request):
+            return .requestParameters(parameters: request, encoding: URLEncoding.default)
+        }
+    }
+    
+    var headers: [String : String]? {
+        return ["Content-Type": "application/x-www-form-urlencoded"]
+    }
+}
